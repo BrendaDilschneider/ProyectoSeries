5039ade9b9f3bec849e87e4a4b329b53ee059da6
BrendaDilschneider <brenda_dilschneider@hotmail.com>
2017-02-16 11:09:02 -0300

    Creando activity para buscar una serie

--- app/src/main/java/series/proyectoseries/MainActivity.java
+++ app/src/main/java/series/proyectoseries/MainActivity.java
@@ -1,7 +1,10 @@
 package series.proyectoseries;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button et1 = (Button)findViewById(R.id.buscar_series);
+        et1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+
+                Intent intent = new Intent(view.getContext(),ActivityBuscarSerie.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
